37497cc233cfefd5cd118f4e0d36ed84f5fb337b
kakashinho <joaovitor.s.c.siqueira@gmail.com>
2025-10-16 23:30:54 -0300

    Adiciona pasta utils com funções utilitárias

---
+++ utils/dataStore.js
@@ -0,0 +1,29 @@
+const disciplinasData = [];
+
+const state = {
+    GITHUB_USERNAME: "kakashinho",
+    LANGUAGE_COLORS: {
+        'JavaScript': '#f1e05a',
+        'Python': '#3572A5',
+        'Java': '#b07219',
+        'TypeScript': '#2b7489',
+        'HTML': '#e34c26',
+        'CSS': '#563d7c',
+        'C': '#555555',
+        'C++': '#f34b7d',
+        'C#': '#178600',
+        'PHP': '#4F5D95',
+        'Ruby': '#701516',
+        'Swift': '#ffaa45',
+        'Go': '#00ADD8',
+        'default': '#ededed'
+    }
+};
+
+const projetosData = [];
+
+module.exports = {
+    disciplinasData,
+    projetosData,
+    state
+};
